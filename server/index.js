072be3bdcbff9639451cbe1ed2aaea2578e44ca0
Arpan Banik <arpanbanik976@gmail.com>
2026-05-01 11:00:05 +0530

    Add and fix index.js file

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5173',
   'http://localhost:3000',
   'http://localhost:5000',
-  'https://arpan-dev-six.vercel.app',
+  'https://arpan-two.vercel.app',
   'https://arpan-banik-portfolio.vercel.app'
 ];
 
